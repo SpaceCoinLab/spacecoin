609f8a7c5279e3da334ffbb5fbed6a34578ec605
SpaceCoinLab <info@spacecoin.xyz>
2015-08-31 00:30:22 +0200

    source

---
+++ src/qt/overviewpage.cpp
@@ -0,0 +1,258 @@
+#include "overviewpage.h"
+#include "ui_overviewpage.h"
+
+#include "walletmodel.h"
+#include "bitcoinunits.h"
+#include "optionsmodel.h"
+#include "transactiontablemodel.h"
+#include "transactionfilterproxy.h"
+#include "guiutil.h"
+#include "guiconstants.h"
+
+#include <QAbstractItemDelegate>
+#include <QPainter>
+#include <QLabel>
+#include <QFrame>
+#include <QStaticText>
+#include <QFontDatabase>
+
+#define DECORATION_SIZE 64
+#define NUM_ITEMS 3
+
+class TxViewDelegate : public QAbstractItemDelegate
+{
+    Q_OBJECT
+public:
+    TxViewDelegate(): QAbstractItemDelegate(), unit(BitcoinUnits::BTC)
+    {
+    }
+    inline void paint(QPainter *painter, const QStyleOptionViewItem &option,
+                      const QModelIndex &index ) const
+    {
+        painter->save();
+
+        QIcon icon = qvariant_cast<QIcon>(index.data(Qt::DecorationRole));
+        QRect mainRect = option.rect;
+        QRect decorationRect(mainRect.topLeft(), QSize(DECORATION_SIZE, DECORATION_SIZE));
+        int xspace = DECORATION_SIZE + 8;
+        int ypad = 6;
+        int halfheight = (mainRect.height() - 2*ypad)/2;
+        QRect amountRect(mainRect.left() + xspace, mainRect.top()+ypad, mainRect.width() - xspace, halfheight);
+        QRect addressRect(mainRect.left() + xspace, mainRect.top()+ypad+halfheight, mainRect.width() - xspace, halfheight);
+        icon.paint(painter, decorationRect);
+
+        QDateTime date = index.data(TransactionTableModel::DateRole).toDateTime();
+        QString address = index.data(Qt::DisplayRole).toString();
+        qint64 amount = index.data(TransactionTableModel::AmountRole).toLongLong();
+        bool confirmed = index.data(TransactionTableModel::ConfirmedRole).toBool();
+        QVariant value = index.data(Qt::ForegroundRole);
+        QColor foreground = option.palette.color(QPalette::Text);
+        if(qVariantCanConvert<QColor>(value))
+        {
+            foreground = qvariant_cast<QColor>(value);
+        }
+
+        painter->setPen(foreground);
+
+        if (fontID > 0)
+        {
+            QString fUbuntu = QFontDatabase::applicationFontFamilies(fontID).at(0);
+            QFont* Ubuntu = new QFont(fUbuntu, 8, QFont::Normal, false);
+            painter->setFont(*Ubuntu);
+
+        }
+
+        painter->drawText(addressRect, Qt::AlignLeft|Qt::AlignVCenter, address);
+
+        if(amount < 0)
+        {
+            foreground = COLOR_NEGATIVE;
+        }
+        else if(!confirmed)
+        {
+            foreground = COLOR_UNCONFIRMED;
+        }
+        else
+        {
+            foreground = option.palette.color(QPalette::Text);
+        }
+        painter->setPen(foreground);
+        QString amountText = BitcoinUnits::formatWithUnit(unit, amount, true);
+        if(!confirmed)
+        {
+            amountText = QString("[") + amountText + QString("]");
+        }
+        painter->drawText(amountRect, Qt::AlignRight|Qt::AlignVCenter, amountText);
+
+        painter->setPen(option.palette.color(QPalette::Text));
+        painter->drawText(amountRect, Qt::AlignLeft|Qt::AlignVCenter, GUIUtil::dateTimeStr(date));
+
+        painter->restore();
+    }
+
+    inline QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
+    {
+        return QSize(DECORATION_SIZE, DECORATION_SIZE);
+    }
+
+    int unit;
+    int fontID;
+
+};
+#include "overviewpage.moc"
+
+OverviewPage::OverviewPage(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::OverviewPage),
+    currentBalance(-1),
+    currentStake(0),
+    currentUnconfirmedBalance(-1),
+    currentConfirmingBalance(-1),
+    currentImmatureBalance(-1),
+    txdelegate(new TxViewDelegate()),
+    filter(0)
+{
+    ui->setupUi(this);
+
+    txdelegate->fontID = -1;
+
+    QFont overviewHeaders("Ubuntu", 10, QFont::Normal);
+    QFont overviewSpend("Ubuntu", 20, QFont::Normal);
+    QFont overviewBalances("Ubuntu", 14, QFont::Normal);
+
+    ui->label->setFont(overviewHeaders);
+    ui->label_3->setFont(overviewHeaders);
+    ui->label_4->setFont(overviewHeaders);
+    ui->label_6->setFont(overviewHeaders);
+    ui->labelImmatureText->setFont(overviewHeaders);
+    ui->labelTotalText->setFont(overviewHeaders);
+
+    ui->labelBalance->setFont(overviewSpend);
+    ui->labelBalance->setContentsMargins(0,0,0,5);
+    ui->labelStake->setFont(overviewBalances);
+    ui->labelStake->setContentsMargins(0,0,0,5);
+    ui->labelImmature->setFont(overviewBalances);
+    ui->labelImmature->setContentsMargins(0,0,0,5);
+    ui->labelUnconfirmed->setFont(overviewBalances);
+    ui->labelUnconfirmed->setContentsMargins(0,0,0,5);
+    ui->labelTotal->setFont(overviewBalances);
+    ui->labelTotal->setContentsMargins(0,0,0,5);
+
+    // Recent transactions
+    ui->listTransactions->setItemDelegate(txdelegate);
+    ui->listTransactions->setIconSize(QSize(DECORATION_SIZE, DECORATION_SIZE));
+    ui->listTransactions->setMinimumHeight(NUM_ITEMS * (DECORATION_SIZE + 2));
+    ui->listTransactions->setAttribute(Qt::WA_MacShowFocusRect, false);
+
+    connect(ui->listTransactions, SIGNAL(clicked(QModelIndex)), this, SLOT(handleTransactionClicked(QModelIndex)));
+
+    // init "out of sync" warning labels
+    ui->labelWalletStatus->setText("(" + tr("out of sync") + ")");
+    ui->labelTransactionsStatus->setText("(" + tr("out of sync") + ")");
+
+    // start with displaying the "out of sync" warnings
+    showOutOfSyncWarning(true);
+}
+
+void OverviewPage::handleTransactionClicked(const QModelIndex &index)
+{
+    if(filter)
+        emit transactionClicked(filter->mapToSource(index));
+}
+
+OverviewPage::~OverviewPage()
+{
+    delete ui;
+}
+
+void OverviewPage::setBalance(qint64 balance, qint64 stake, qint64 unconfirmedBalance, qint64 confirmingBalance, qint64 immatureBalance)
+{
+    int unit = model->getOptionsModel()->getDisplayUnit();
+    currentBalance = balance;
+    currentStake = stake;
+    currentUnconfirmedBalance = unconfirmedBalance;
+    currentConfirmingBalance = confirmingBalance;
+    currentImmatureBalance = immatureBalance;
+    ui->labelBalance->setText(BitcoinUnits::formatWithUnit(unit, balance));
+    ui->labelStake->setText(BitcoinUnits::formatWithUnit(unit, stake));
+    ui->labelUnconfirmed->setText(BitcoinUnits::formatWithUnit(unit, unconfirmedBalance));
+    ui->labelImmature->setText(BitcoinUnits::formatWithUnit(unit, immatureBalance));
+    ui->labelTotal->setText(BitcoinUnits::formatWithUnit(unit, balance + stake + unconfirmedBalance + immatureBalance));
+
+
+    if (confirmingBalance > unconfirmedBalance)
+    {
+            ui->label_3->setText("Confirming");
+            ui->labelUnconfirmed->setText(BitcoinUnits::formatWithUnit(unit, confirmingBalance));
+    } else {
+            if (ui->label_3->text() != "Unconfirmed")
+                ui->label_3->setText("Unconfirmed");
+    }
+
+    // only show immature (newly mined) balance if it's non-zero, so as not to complicate things
+    // for the non-mining users
+    bool showUnconfirmed = false;
+
+    if(confirmingBalance !=0 || unconfirmedBalance !=0)
+        showUnconfirmed = true;
+
+    bool showImmature = immatureBalance != 0;
+
+    ui->label_3->setVisible(showUnconfirmed);
+    ui->labelUnconfirmed->setVisible(showUnconfirmed);
+    ui->labelImmature->setVisible(showImmature);
+    ui->labelImmatureText->setVisible(showImmature);
+}
+
+void OverviewPage::setModel(WalletModel *model)
+{
+    this->model = model;
+    if(model && model->getOptionsModel())
+    {
+        // Set up transaction list
+        filter = new TransactionFilterProxy();
+        filter->setSourceModel(model->getTransactionTableModel());
+        filter->setLimit(NUM_ITEMS);
+        filter->setDynamicSortFilter(true);
+        filter->setSortRole(Qt::EditRole);
+        filter->setShowInactive(false);
+        filter->sort(TransactionTableModel::Status, Qt::DescendingOrder);
+
+        ui->listTransactions->setModel(filter);
+        ui->listTransactions->setModelColumn(TransactionTableModel::ToAddress);
+
+        // Keep up to date with wallet
+        setBalance(model->getBalance(), model->getStake(), model->getUnconfirmedBalance(), model->getConfirmingBalance(), model->getImmatureBalance());
+        connect(model, SIGNAL(balanceChanged(qint64, qint64, qint64, qint64, qint64)), this, SLOT(setBalance(qint64, qint64, qint64, qint64, qint64)));
+
+        connect(model->getOptionsModel(), SIGNAL(displayUnitChanged(int)), this, SLOT(updateDisplayUnit()));
+    }
+
+    // update the display unit, to not use the default ("BTC")
+    updateDisplayUnit();
+}
+
+void OverviewPage::updateDisplayUnit()
+{
+    if(model && model->getOptionsModel())
+    {
+        if(currentBalance != -1)
+            setBalance(currentBalance, model->getStake(), currentUnconfirmedBalance, currentConfirmingBalance, currentImmatureBalance);
+
+        // Update txdelegate->unit with the current unit
+        txdelegate->unit = model->getOptionsModel()->getDisplayUnit();
+        if (txdelegate->fontID < 0)
+        {
+            FontID = QFontDatabase::addApplicationFont(":/fonts/Ubuntu-Medium");
+            txdelegate->fontID = FontID;
+        }
+
+        ui->listTransactions->update();
+    }
+}
+
+void OverviewPage::showOutOfSyncWarning(bool fShow)
+{
+    ui->labelWalletStatus->setVisible(fShow);
+    ui->labelTransactionsStatus->setVisible(fShow);
+}
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define SpaceCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "SpaceCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
